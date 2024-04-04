07f295461a29fa0dc140a15fb80f9f5fd362ebc2
Charan-bavaji <charan.r7760@gmail.com>
2024-04-04 21:11:00 +0530

    Updated insta link

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -21,7 +21,7 @@ const SocialMedia = () => {
                     </a>
                 </li>
                 <li className="p-2 border-2 rounded-full border-slate-400 bg-slate-200 transition duration-150 ease-in-out hover:-translate-y-2">
-                    <a href="https://www.instagram.com/hacker_zakc/">
+                    <a href="https://www.instagram.com/code_201/">
                         <img src={insta} alt="insta" width={30} height={30} />
                     </a>
                 </li>
